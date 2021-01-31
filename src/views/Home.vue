edf19f10e86ea12e1d2a1258bc8cb64a49b8725c
messenger <269170236@qq.com>
2021-01-31 15:56:00 +0800

    '导航栏'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <router-view></router-view>
+    <tabBar />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import tabBar from '../components/TabBar.vue';
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld,
+    tabBar,
+  },
+  data() {
+    return {};
   },
 };
 </script>
+
+<style lang="scss">
+
+</style>
